bc53e21f07f56fcfa47c73022c9a22903d8a14cb
Asiri_Maduranga <Asirimadhuranga2022@gmail.com>
2025-03-27 00:35:46 +0530

    Add files via upload

---
+++ Students.js
@@ -0,0 +1,9 @@
+let students= [
+    {id: "2021ict01", Name: "Lakpreeth", Age: 23, Gender: "male"},
+    {id: "2021ict02", Name: "Omeshi", Age: 25, Gender: "female"},
+    {id: "2021ict03", Name: "Kavindu", Age: 24, Gender: "male"},
+    {id: "2021ict04", Name: "Varunika", Age: 23, Gender: "female"},
+    {id: "2021ict05", Name: "Yasas", Age: 24, Gender: "male"}
+];
+
+module.exports=students;
---
+++ app.js
@@ -0,0 +1,41 @@
+const express= require("express");
+const app= express();
+const port= 3002;
+
+const students= require('./DB/Students.js');
+
+app.get('/stu', (req, res) => {
+    res.send(students);
+});
+
+app.get('/stu/:id', (req, res) => {
+    const id= req.params.id;
+    const result= students.find(student => student.id == id);
+    // 
+    if(result != null){
+        res.send(result);
+    }else{
+        res.status(404).send("Student not found...");
+    }
+    
+});
+
+app.get('/stu/gender/:gen', (req, res) => {
+    const gender= req.params.gen;
+    const result= students.filter(student => student.Gender == gender);
+    res.send(result);
+})
+
+app.get('/', (req, res)=>{
+    res.send("Hello Express JS");
+});
+
+app.get('/msg', (req, res)=>{
+    res.send("Hello IT Students.");
+});
+
+app.listen(port, ()=> {
+        console.log(`Server is running on ${port}`);
+});
+
+
